6549dd931576ccb42838eb186c96ea737d330e6b
chistyhimel <fc_himel@yahoo.com>
2021-06-13 22:22:47 +0600

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,143 @@
+import React, { useEffect, useState } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/ToolBar";
+import Button from "@material-ui/core/Button";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
+
+function ElevationScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: "3em",
+  },
+  logoContainer: {
+    padding: 0,
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+  },
+  logo: {
+    height: "7em",
+  },
+  tabContainer: {
+    marginLeft: "auto",
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px",
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: "50px",
+    marginLeft: "50px",
+    marginRight: "25px",
+  },
+}));
+
+function Header(props) {
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e, value) => {
+    setValue(value);
+  };
+
+  useEffect(() => {
+    if ((window.location.pathname === "/") & (value !== 0)) {
+      setValue(0);
+    } else if ((window.location.pathname === "/services") & (value !== 1)) {
+      setValue(1);
+    } else if ((window.location.pathname === "/revolution") & (value !== 2)) {
+      setValue(2);
+    } else if ((window.location.pathname === "/about") & (value !== 3)) {
+      setValue(3);
+    } else if ((window.location.pathname === "/contact") & (value !== 4)) {
+      setValue(4);
+    }
+  }, [value]);
+
+  return (
+    <>
+      <ElevationScroll>
+        <AppBar position="fixed">
+          <ToolBar disableGutters>
+            <Button
+              disableRipple
+              component={Link}
+              to="/"
+              className={classes.logoContainer}
+              onClick={() => setValue(0)}
+            >
+              <img src={logo} alt="company logo" className={classes.logo} />
+            </Button>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={classes.tabContainer}
+            >
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/"
+                label="Home"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/services"
+                label="Services"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/revolution"
+                label="The Revolution"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/about"
+                label="About Us"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/contact"
+                label="Contact Us"
+              />
+            </Tabs>
+            <Button
+              variant="contained"
+              color="secondary"
+              size="large"
+              className={classes.button}
+            >
+              Free Estimate
+            </Button>
+          </ToolBar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </>
+  );
+}
+
+export default Header;
